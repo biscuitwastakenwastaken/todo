571d2d62e4730a0881b316665c6b1bdd5429de71
Matt Mayfield <mattmayf411@gmail.com>
2021-10-06 19:20:50 -0500

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        successGreen: "#9CE27C",
+        warningYellow: "#E0E27C",
+        negativeRed: "#E27C7C",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
